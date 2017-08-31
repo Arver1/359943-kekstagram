1d9f6bae93a68da2d0497f9d3e5367072e7f2939
Arver1 <aactepanov@mail.ru>
2017-09-01 00:08:10 +0300

    fixed bugs

--- js/pictures.js
+++ js/pictures.js
@@ -7,44 +7,40 @@ var comments = [
   'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
   'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
 ];
-var pictures = [];
-var template;
 var firstElement;
 var getRandomNumber = function (min, max) {
-  var rand = min + Math.random * (max - min + 1);
-  rand = Math.floor(rand);
-  return rand;
+  return Math.floor(min + Math.random() * (max - min + 1));
 };
-var createArrayJsObjects = function (number) {
-  for (var i = 0; i < number; i++) {
+var hideElement = function (className) {
+  document.querySelector(className).classList.add('hidden');
+};
+var createPictures = function (countPictures) {
+  var pictures = [];
+  for (var i = 0; i < countPictures; i++) {
     pictures[i] = {
       url: 'photos/{{' + i + '}}.jpg',
       likes: getRandomNumber(15, 200),
       randomComments: [comments[getRandomNumber(0, 5)], comments[getRandomNumber(0, 5)]]
     };
   }
+  return pictures;
 };
-var createDOMElementsInBlock = function (block) {
-  var div = document.querySelector(block);
+var appendPictures = function (container, pictures, templateId) {
+  var template = document.getElementById(templateId).content;
+  var div = document.querySelector(container);
   for (var i = 0; i < pictures.length; i++) {
     var element = template.cloneNode(true);
     element.querySelector('img').src = pictures[i].url;
     element.querySelector('.picture-likes').textContent = pictures[i].likes;
-    element.querySelector('.picture-comments').textContent = pictures[i].randomComments.length.toString();
+    element.querySelector('.picture-comments').textContent = pictures[i].randomComments.length;
     div.appendChild(element);
   }
 };
-var hideElement = function (className) {
-  var element = document.querySelector(className);
-  if (!element.classList.contains('hidden')) {
-    element.classList.add('hidden');
-  }
-};
-createArrayJsObjects(25);
-template = document.getElementById('picture-template').content;
-createDOMElementsInBlock('.pictures');
+var pictures = createPictures(25);
+appendPictures('.pictures', pictures, 'picture-template');
 hideElement('.upload-overlay');
-firstElement = document.querySelector('.gallery-overlay').classList.remove('hidden');
+firstElement = document.querySelector('.gallery-overlay');
+firstElement.classList.remove('hidden');
 firstElement.querySelector('.gallery-overlay-image').src = pictures[0].url;
 firstElement.querySelector('.likes-count').textContent = pictures[0].likes;
 firstElement.querySelector('.comments-count').textConent = pictures[0].randomComments.length;
